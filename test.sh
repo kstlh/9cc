b5d407a550d20f57d71c26d9d7839b5e48625ef6
kstlh <kstlhfdt@gmail.com>
2018-11-24 02:05:56 +0900

    test.shのメッセージを変更

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ try() {
   ./tmp
   actual="$?"
 
-  if [ "$actual" != "$expected" ]; then
-    echo "$input expected, but got $actual"
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$input => expected $expected, but got $actual"
     exit 1
   fi
 }
