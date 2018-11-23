365905c8e2d64361358c00fe81496982905959c1
kstlh <kstlhfdt@gmail.com>
2018-11-23 23:23:21 +0900

    パーサを導入

--- 9cc.c
+++ 9cc.c
@@ -16,9 +16,86 @@ typedef struct {
   char *input; // トークン文字列（エラーメッセージ用）
 } Token;
 
+enum {
+      ND_NUM = 256
+};
+
+typedef struct Node {
+  int ty;
+  struct Node *lhs;
+  struct Node *rhs;
+  int val;
+} Node;
+
 // トークナイズした結果のトークン列はこの配列に保存する
 // 100個以上のトークンは来ないものとする
 Token tokens[100];
+
+int pos = 0;
+
+Node *new_node(int ty, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *mul();
+Node *term();
+
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr() {
+  Node *lhs = mul();
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+Node *mul() {
+  Node *lhs = term();
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+  return lhs;
+}
+
+Node *term() {
+  if (tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+  if (tokens[pos].ty == '(') {
+    pos++;
+    Node *node = expr();
+    if (tokens[pos].ty != ')') 
+      fprintf(stderr, "開きカッコに対応する閉じカッコがありません: %s",
+	    tokens[pos].input);
+    pos++;
+    return node;
+  }
+  fprintf(stderr,"数値でも開きカッコでもないトークンです: %s" ,
+	tokens[pos].input);
+}
 
 // pが指している文字列をトークンに分割してtokensに保存する
 void tokenize(char *p) {
@@ -70,6 +147,8 @@ int main(int argc, char **argv) {
   // トークナイズする
   tokenize(argv[1]);
 
+  Node *node = expr();
+
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
@@ -80,32 +159,6 @@ int main(int argc, char **argv) {
   if (tokens[0].ty != TK_NUM)
     error(0);
   printf("  mov rax, %d\n", tokens[0].val);
-
-  // `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつ
-  // アセンブリを出力
-  int i = 1;
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-	error(i);
-      printf("  add rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-	error(i);
-      printf("  sub rax, %d\n", tokens[i].val);
-      i++;
-      continue;
-    }
-
-    error(i);
-  }
-
   printf("  ret\n");
   return 0;
 }
