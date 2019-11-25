b677fc20bd6a92d5f591ed85005bb41a6c306950
kstlh <kstlhfdt@gmail.com>
2019-11-26 02:51:05 +0900

    関数パラメータ追加

--- 9cc.h
+++ 9cc.h
@@ -107,7 +107,7 @@ Vector *parse(Vector *tokens);
 
 enum {
       IR_IMM = 256,
-      IR_ADD_IMM,
+      IR_SUB_IMM,
       IR_MOV,
       IR_RETURN,
       IR_CALL,
@@ -117,6 +117,7 @@ enum {
       IR_LOAD,
       IR_STORE,
       IR_KILL,
+      IR_SAVE_ARGS,
       IR_NOP,
 };
 
@@ -137,6 +138,7 @@ typedef struct {
 enum {
       IR_TY_NOARG,
       IR_TY_REG,
+      IR_TY_IMM,
       IR_TY_LABEL,
       IR_TY_REG_REG,
       IR_TY_REG_IMM,
